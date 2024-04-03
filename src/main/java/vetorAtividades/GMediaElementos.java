7d4bc2f1d04940b6436f2716b4bd3afa48cf6e69
Carolina Campos <carolinacampos.dev@gmail.com>
2024-04-03 17:06:14 -0300

    Media dos elementos

--- src/main/java/vetorAtividades/GMediaElementos.java
+++ src/main/java/vetorAtividades/GMediaElementos.java
@@ -1,4 +1,17 @@
 package vetorAtividades;
 
 public class GMediaElementos {
+    public static void main(String[] args) {
+        int[] vetor = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        int soma = 0;
+        for (int i = 0;i<vetor.length;i++) {
+            soma += vetor[i];
+        }
+        float media = (float) soma/vetor.length;
+            System.out.println("A média é: " + media );
+
+}
+
+
 }
