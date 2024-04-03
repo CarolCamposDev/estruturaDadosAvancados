63f7b3e5ba36dee455637e479658f0b76027a793
Carolina Campos <carolinacampos.dev@gmail.com>
2024-04-03 17:04:16 -0300

    Vetor com 10 posições

--- src/main/java/vetorAtividades/BVetor10Posicoes.java
+++ src/main/java/vetorAtividades/BVetor10Posicoes.java
@@ -3,5 +3,8 @@ package vetorAtividades;
 public class BVetor10Posicoes {
     public static void main(String[] args) {
         int[] vetor = new int[10];
+
+        System.out.println(vetor.length);
+
     }
 }
