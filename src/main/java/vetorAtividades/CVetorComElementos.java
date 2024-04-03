f508f6fc4244ea5cf37c8374acc373fd570929c3
Carolina Campos <carolinacampos.dev@gmail.com>
2024-04-03 17:04:40 -0300

    Vetor com elementos

--- src/main/java/vetorAtividades/CVetorComElementos.java
+++ src/main/java/vetorAtividades/CVetorComElementos.java
@@ -4,5 +4,8 @@ public class CVetorComElementos {
     public static void main(String[] args) {
         int[] vetor = {1, 3, 5, 7, 9};
 
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
     }
 }
