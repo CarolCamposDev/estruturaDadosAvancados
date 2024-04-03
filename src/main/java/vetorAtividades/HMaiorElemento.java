6bc1b394ca2bfc17229e45fcc59d20184a533e96
Carolina Campos <carolinacampos.dev@gmail.com>
2024-04-03 17:06:25 -0300

    Maior elemento

--- src/main/java/vetorAtividades/HMaiorElemento.java
+++ src/main/java/vetorAtividades/HMaiorElemento.java
@@ -1,4 +1,16 @@
 package vetorAtividades;
 
 public class HMaiorElemento {
+    public static void main(String[] args) {
+        int[] vetor = {1, 4, 6, 9, 21, 33, 98, 209, 444, 334, 76, 189};
+
+        int maior = vetor[0];
+
+        for (int i = 1; i < vetor.length; i++) {
+            if (vetor[i] > maior) {
+                maior = vetor[i];
+            }
+        }
+        System.out.println(maior);
+    }
 }
