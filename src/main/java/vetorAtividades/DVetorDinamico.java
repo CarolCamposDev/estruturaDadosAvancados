6f237889e6501ef2a4003f7068f1c6520eec51b2
Carolina Campos <carolinacampos.dev@gmail.com>
2024-04-03 17:04:55 -0300

    Vetor dinâmico

--- src/main/java/vetorAtividades/DVetorDinamico.java
+++ src/main/java/vetorAtividades/DVetorDinamico.java
@@ -1,12 +1,30 @@
 package vetorAtividades;
 
+import static java.awt.AWTEventMulticaster.add;
+
 public class DVetorDinamico {
-
     public static void main(String[] args) {
-        int [] vetor = {0};
+        int[] vetor = new int[0];
+        vetor = add(vetor, 1);
+        vetor = add(vetor, 2);
+        vetor = add(vetor, 3);
+        vetor = add(vetor, 4);
+        vetor = add(vetor, 5);
 
-
+        for (int elemento : vetor) {
+            System.out.println(elemento);
+        }
     }
 
+    public static int[] add(int[] vetorAntigo, int valor) {
+        int[] vetorNovo = new int[vetorAntigo.length + 1];
+
+        for (int i = 0; i < vetorAntigo.length; i++) {
+            vetorNovo[i] = vetorAntigo[i];
+        }
+
+        vetorNovo[vetorAntigo.length] = valor;
+        return vetorNovo;
+    }
 
 }
