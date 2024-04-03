18d64654553758b0eeb376ae786c707a19695938
Carolina Campos <carolinacampos.dev@gmail.com>
2024-04-03 17:06:05 -0300

    Somar elementos

--- src/main/java/vetorAtividades/FSomaElementos.java
+++ src/main/java/vetorAtividades/FSomaElementos.java
@@ -1,4 +1,14 @@
 package vetorAtividades;
 
 public class FSomaElementos {
+    public static void main(String[] args) {
+        int[] vetor = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        int soma = 0;
+        for (int i = 0;i<vetor.length;i++) {
+            soma += vetor[i];
+        }
+
+        System.out.println("A soma é: " + soma);
+    }
 }
