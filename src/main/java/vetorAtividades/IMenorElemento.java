787aca57bcc05384d208c1b0fe9ad0c97d1257e9
Carolina Campos <carolinacampos.dev@gmail.com>
2024-04-03 17:06:32 -0300

    Menor elemento

--- src/main/java/vetorAtividades/IMenorElemento.java
+++ src/main/java/vetorAtividades/IMenorElemento.java
@@ -1,4 +1,16 @@
 package vetorAtividades;
 
 public class IMenorElemento {
+    public static void main(String[] args) {
+        int[] vetor = {1, 4, 6, 9, 21, 33, 98, 209, 444, 334, 76, 189};
+        int menor = vetor[0];
+
+        for (int i = 1; i < vetor.length; i++) {
+            if (vetor[i] < menor) {
+                menor = vetor[i];
+            }
+        }
+        System.out.println(menor);
+    }
+
 }
